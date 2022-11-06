0573f7a9cee236358173de83bb2cd110ee5827a2
Michał Nowak <michal.nowak@appunite.com>
2022-11-06 23:52:31 +0100

    fix: use kiwi to to get stations repository

--- lib/features/stations/presentation/notifiers/stations_notifier.dart
+++ lib/features/stations/presentation/notifiers/stations_notifier.dart
@@ -1,15 +1,14 @@
-import 'package:air_quality/core/data/network_manager.dart';
 import 'package:air_quality/core/domain/exceptions/aq_exception.dart';
 import 'package:air_quality/core/domain/exceptions/unknown_exception.dart';
 import 'package:air_quality/core/domain/state/data_state.dart';
+import 'package:air_quality/dependencies.dart';
 import 'package:air_quality/features/stations/data/stations_repository.dart';
 import 'package:air_quality/features/stations/domain/entities/station_entity.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final stationsProvider =
     StateNotifierProvider.autoDispose<StateNotifier<DataState<List<StationEntity>>>, DataState<List<StationEntity>>>(
-        (_) => StationsNotifier(StationsRepository(NetworkManager(Dio()))));
+        (_) => StationsNotifier(kiwi<StationsRepository>()));
 
 class StationsNotifier extends StateNotifier<DataState<List<StationEntity>>> {
   StationsNotifier(this._stationsRepository) : super(const DataState.loading()) {
